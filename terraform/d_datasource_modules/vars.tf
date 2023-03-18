8b084cc3e2628b115f264b7719dcf9203d360072
Floodnut <gsniper777@gmail.com>
2023-03-18 23:34:57 +0900

    feat: datasource

---
+++ terraform/d_datasource_modules/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {}
+
+variable "AMIS" {
+    type = map(string)
+    default = {
+        ap-northeast-2 = "ami-035e3e44dc41db6a2"
+    }
+}

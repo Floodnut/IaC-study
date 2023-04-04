b7ce07dd58932d41317eb4e84a058831e023ed7c
Floodnut <gsniper777@gmail.com>
2023-04-04 23:44:57 +0900

    feat: Terraform Packer를 통한 빌드 실습

---
+++ terraform/g_packer/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "__YOUR_PRIVATE_KEY__"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "__YOUR_PUBLIC_KEY__"
+}
+
